f58c4b3e16b97f8863b336e4a2d35f0fab281938
andrey-hohlov <andr.hohlov@gmail.com>
2017-01-16 19:07:19 +0700

    add karma

---
+++ karma.conf.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpackConfig = require('./webpack.config.js');
+
+module.exports = function(config) {
+  config.set({
+    basePath: './',
+    frameworks: ['mocha', 'chai'],
+    files: [
+      'test/**/*.js',
+      'test/**/*.html',
+      'src/Tabit.js',
+    ],
+    exclude: [],
+    preprocessors: {
+      'test/**/*.html': ['html2js'],
+      'test/**/*.js': ['babel'],
+      'src/Tabit.js': ['webpack']
+    },
+    babelPreprocessor: {
+      options: {
+        presets: ['es2015']
+      }
+    },
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+    reporters: ['mocha'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: true,
+    concurrency: Infinity,
+
+    //  Custom launcher for Travis-CI
+    customLaunchers: {
+      chromeTravisCI: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    }
+  });
+
+  if (process.env.TRAVIS) {
+    config.browsers = ['chromeTravisCI'];
+  }
+};
